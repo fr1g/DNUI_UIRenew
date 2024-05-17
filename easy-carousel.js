468a05c57be6a97789d25bcc9cd1d5d8601dd26a
Feiron <53085628+fr1g@users.noreply.github.com>
2024-05-17 12:13:59 +0800

    initial Map page

--- easy-carousel.js
+++ easy-carousel.js
@@ -1,18 +1,18 @@
 var carouselInterval, 
     carouselImageList = [], 
     carouselHostElement, 
-    carouselCurrentValue = 0;
-
-const carouselItemHtmlString = `
-    <div id="carousel::#CAR_INDEX#" class="max-h-screen carousels bg-center bg-no-repeat bg-cover bg-fill opacity-30 absolute top-0 bottom-0 left-0 right-0" 
-         style="background-image: url(#IMG_PATH#) !important; background-color: darkgray" >
+    carouselCurrentValue = 0
+    carouselItemHtmlString = `
+    <div id="carousel::#CAR_INDEX#" class="#CLASS#  max-h-screen carousels bg-center bg-no-repeat bg-cover bg-fill opacity-30 absolute top-0 bottom-0 left-0 right-0" 
+         style="background-image: url(#IMG_PATH#) !important; background-color: darkgray; #OPACITY#" >
     </div>
 `;
 
-function Initial(hostElementId, list) {
+function Initial(hostElementId, list, classname = null, opacity = null) {
     carouselHostElement = document.getElementById(hostElementId.replace('#', ''));
     carouselHostElement.style.position = 'relative';
     let index = 0;
+    carouselItemHtmlString = carouselItemHtmlString.replace('#CLASS#', classname ?? '').replace('#OPACITY#', (opacity ?? `opacity: ${opacity} !important`));
     carouselImageList = list;
     for(let car of carouselImageList){
         carouselHostElement.innerHTML += `${carouselItemHtmlString}\n`.replace('#IMG_PATH#', car).replace('#CAR_INDEX#', index++);
@@ -28,10 +28,10 @@ function ChangeCarousel() {
     if (carouselCurrentValue < carouselImageList.length - 1) carouselCurrentValue++;
     else carouselCurrentValue = 0;
     // foreach remove all carousel show status then set the next image to display.
-    console.log(`changing carousel: ${carouselCurrentValue}`)
+    // console.log(`changing carousel: ${carouselCurrentValue}`)
     for(let sub of carouselHostElement.children){
         if(sub.id == `carousel::${carouselCurrentValue}`){
-            console.log('proc')
+            // console.log('proc')
             document.getElementById(sub.id).classList.remove('fade-au');
             document.getElementById(sub.id).classList.add('fade-in');
         }else{
