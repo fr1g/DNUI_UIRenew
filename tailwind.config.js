373cdeb08798c3313ad2bff95d24bf2759faae00
Feiron <53085628+fr1g@users.noreply.github.com>
2024-05-24 09:06:05 +0800

    更换颜色 (还是得换css)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   ],
   theme: { 
     extend: {
-      
+      colors: {
+        // 'main': '#1d4894',
+        'main-50':   '#dde9ff',
+        'main-100':   '#c8dafc',
+        'main-200':   '#afcaf8',
+        'main-300':   '#91b6f5',
+        'main-400':   '#77a2ec',
+        'main-500':   '#5f90e5',
+        'main-600':   '#4a81e1',
+        'main-700':   '#4073cd',
+        'main-800':   '#2d60b9',
+        'main-900':   '#1d4894',
+        'main-950':   '#113676',
+      }
     },
   },
   plugins: [], 
